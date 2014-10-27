f3ee396343a2cdde5d092173131e5327260b0dd1
Timothy Sherlock <tsherloc@cs.utexas.edu>
2014-10-27 16:28:53 -0500

    new pull and playing around

--- project_3/lyee_test.py
+++ project_3/lyee_test.py
@@ -3,31 +3,32 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('test_data/ball_2.mov')
+cap = cv2.VideoCapture('test_data/ball_3.mov')
 
-fgbg = cv2.BackgroundSubtractorMOG()
+fgbg = cv2.BackgroundSubtractorMOG(200, 10, 0.7, 0)
 
+ret, frame = cap.read()
+init_fgmask = fgbg.apply(frame)
 while(1):
     ret, frame = cap.read()
-
     fgmask = fgbg.apply(frame)
     
-    im = fgmask
-    #print fgmask.shape
+    im = fgmask - init_fgmask
     
-    im2 = fgmask #cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(im2,127,255,0)
-    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    # im2 = cv2.filter2D(frame, -1, fgmask) #cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    # ret,thresh = cv2.threshold(im,127,255,cv2.THRESH_BINARY)
+    # contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    for i in range(0, len(contours)):
-        if (i % 2 == 0):
-           cnt = contours[i]
-           x,y,w,h = cv2.boundingRect(cnt)
-           print x,y,w,h
-           cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
-        
-    cv2.imshow('frame',im2)
-    k = cv2.waitKey(30) & 0xff
+    # print "contours ", len(contours)
+    # for i in range(0, len(contours)):
+    #   # if (i % 2 == 0):
+    #   cnt = contours[i]
+    #   x,y,w,h = cv2.boundingRect(cnt)
+    #   # print x,y,w,h
+    #   cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow('frame',im)
+    # cv2.imshow('original', frame)
+    k = cv2.waitKey(500) & 0xff
     if k == 27:
         break
 
