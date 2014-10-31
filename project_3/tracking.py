3d36c84465ae56b9e82631e1f7deabbdde00efec
Tim Sherlock <tim@wireless-10-147-252-214.public.utexas.edu>
2014-10-30 19:36:25 -0500

    minor fixes

--- project_3/tracking.py
+++ project_3/tracking.py
@@ -10,24 +10,26 @@ import numpy
 
 def findBackground(video):
 
-    _, result = video.read()
-    result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+    _, background = video.read()
+
     i = 1
     n = 100  # Number of frames to use for background estimation
 
     # Read frames and do a weighted sum
     while(i < n):
         i += 1
-        ret, frame = video.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, frame = video.read()
         beta = 1.0/i
         alpha = 1.0 - beta
-        result = cv2.addWeighted(result, alpha, frame, beta, 0.0)
+        background = cv2.addWeighted(background, alpha, frame, beta, 0.0)
 
     # reset video capture to first frame
     video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
 
-    return result
+    cv2.imshow('estimated background', background)
+    cv2.waitKey(3000)
+
+    return background
 
 
 def track_ball_1(video):
@@ -54,21 +56,17 @@ def track_ball_1(video):
             break
 
         orig_frame = frame
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = fgbg.apply(frame)
 
         contours, _ = cv2.findContours(frame, cv2.RETR_TREE,
                                        cv2.CHAIN_APPROX_SIMPLE)
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
 
         sort_cnt = sorted(contours, key=lambda x: cv2.boundingRect(x)[2],
                           reverse=True)
 
         x, y, w, h = cv2.boundingRect(sort_cnt[0])
         result.append((x, y, x+w, y+h))
-        cv2.rectangle(orig_frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
+        cv2.rectangle(orig_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         cv2.imshow('frame', orig_frame)
         cv2.waitKey(30)
@@ -90,7 +88,6 @@ def track_ball_3(video):
 def track_ball_4(video):
     """As track_ball_1, but for ball_2.mov."""
     pass
-
 
 def track_face(video):
     """As track_ball_1, but for face.mov."""
