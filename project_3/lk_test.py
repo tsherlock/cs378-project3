993a7d54f8bf085166b5f2992a183e43c7451835
Tim Sherlock <tim@wireless-10-147-252-214.public.utexas.edu>
2014-10-30 20:11:45 -0500

    lk files

---
+++ project_3/lk_test.py
@@ -0,0 +1,21 @@
+import lktrack
+
+imnames = []
+
+path = "test_data/ball_4_frames/"
+i = 326
+while i > -1:
+	s = str(i)
+	while len(s) < 3:
+		s = "0" + s
+	imnames.append(path + "frame_" + s + ".png")
+	i = i - 1
+
+print imnames
+lkt = lktrack.LKTracker(imnames)
+
+lkt.detect_points()
+lkt.draw()
+for i in range(len(imnames)-1):
+	lkt.track_points()
+	lkt.draw()
