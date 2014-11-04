9b3c4d6a0de71d5575d4e6b323596dcab1354ddc
Lyee Chong <lchong@cs.utexas.edu>
2014-11-03 21:11:42 -0600

    it works

--- project_3/tracking.py
+++ project_3/tracking.py
@@ -52,7 +52,6 @@ def track_ball_1(video):
       a list of (min_x, min_y, max_x, max_y) four-tuples containing the pixel
       coordinates of the rectangular bounding box of the ball in each frame.
     """
-
     result = []
     frames = readFrames(video)
 
@@ -97,64 +96,36 @@ def track_ball_3(video):
     return track_ball_1(video)
 
 
-def track_ball_4(cap):
-    
+def track_ball_4(video):
+    result = []
+    frames = readFrames(video)
 
-# take first frame of the video
-    _,frame = cap.read()
+    i = 0
+    while i < len(frames):
+        frame = frames[i]
 
-# setup initial location of window
-    r,h,c,w = 250,90,400,125  # simply hardcoded the values
-    track_window = (c,r,w,h)
-
-# set up the ROI for tracking
-    roi = frame[r:r+h, c:c+w]
-    hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-    roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-    cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
-
-# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-    term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
-
-    while(1):
-        ret ,frame = cap.read()
-
-        if ret == True:
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-
-        # apply meanshift to get the new location
-            ret, track_window = cv2.CamShift(dst, track_window, term_crit)
-
-        # Draw it on image
-
-            #pts = cv2.boxPoints(ret)
-            #pts = np.int0(pts)
-            #img2 = cv2.polylines(frame,[pts],True, 255,2)
-            contours, _ = cv2.findContours(frame, cv2.RETR_TREE,
+        orig_frame = frame
+        
+        img = cv2.imread('test_data/ball_4_frames/frame_131.png', 0) #change to grayscale param
+        edges = cv2.Canny(frame,700,800)
+        
+        contours, _ = cv2.findContours(edges, cv2.RETR_TREE,
                                        cv2.CHAIN_APPROX_SIMPLE)
 
-            sort_cnt = sorted(contours, key=lambda x: cv2.boundingRect(x)[2],
+        sort_cnt = sorted(contours, key=lambda x: cv2.boundingRect(x)[2],
                           reverse=True)
 
-            x, y, w, h = cv2.boundingRect(sort_cnt[0])
-            result.append((x, y, x+w, y+h))
-            img2 = cv2.rectangle(track_window, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        x, y, w, h = cv2.boundingRect(sort_cnt[0])
+        result.append((x, y, x+w, y+h))
+        cv2.rectangle(orig_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-            cv2.imshow('img2',img2)
+        cv2.imshow("ball", orig_frame)
+        cv2.waitKey(30)
 
-            k = cv2.waitKey(60) & 0xff
-            if k == 27:
-                break
-            else:
-                cv2.imwrite(chr(k)+".jpg",img2)
-
-        else:
-            break
+        i += 1
 
     cv2.destroyAllWindows()
-    cap.release()
+    return result
 
 def track_face(video):
 	"""As track_ball_1, but for face.mov."""
